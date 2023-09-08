4307c639899cdc003465332b055c081c65d5eb93
WildCard-NO <kalex.boe@gmail.com>
2023-09-08 14:40:42 +0200

    grid in js

---
+++ main.js
@@ -0,0 +1,18 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const gridContainer = document.getElementById('grid-container');
+
+    // Define grid size here
+    const gridSize = 16; // Change this value for different grid sizes
+    const cellSize = 50; // This is the pixel size of each cell
+
+    gridContainer.style.gridTemplateColumns = `repeat(${gridSize}, ${cellSize}px)`;
+    gridContainer.style.gridTemplateRows = `repeat(${gridSize}, ${cellSize}px)`;
+    gridContainer.style.width = `${gridSize * cellSize}px`;
+    gridContainer.style.height = `${gridSize * cellSize}px`;
+
+    for (let i = 0; i < gridSize * gridSize; i++) {
+        const cell = document.createElement('div');
+        cell.classList.add('cell');
+        gridContainer.appendChild(cell);
+    }
+});
