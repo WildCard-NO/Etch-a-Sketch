f8cbd6678c990eebc1c663e568a60e09e336b3fb
WildCard-NO <kalex.boe@gmail.com>
2023-09-12 13:27:58 +0200

    clean

--- main.js
+++ main.js
@@ -1,8 +1,33 @@
-const toggleBorders = document.getElementById('toggleBorders');
-const gridContainer = document.getElementById('gridContainer');
-const sizeValue = document.getElementById('sizeValue')
-const sizeSlider = document.getElementById('sizeSlider')
+function createGrid() {
 
+
+
+    const colorPicker = document.getElementById('colorPicker');
+    const toggleBorders = document.getElementById('toggleBorders');
+    const gridContainer = document.getElementById('gridContainer');
+    const sizeValue = document.getElementById('sizeValue');
+    const sizeSlider = document.getElementById('sizeSlider');
+
+
+    // Define grid size here
+    const gridSize = 16; // Change this value for different grid sizes
+    const cellSize = 50; // This is the pixel size of each cell
+
+
+    gridContainer.style.gridTemplateColumns = `repeat(${gridSize}, ${cellSize}px)`;
+    gridContainer.style.gridTemplateRows = `repeat(${gridSize}, ${cellSize}px)`;
+    gridContainer.style.width = `${gridSize * cellSize}px`;
+    gridContainer.style.height = `${gridSize * cellSize}px`;
+
+
+
+    for (let i = 0; i < gridSize * gridSize; i++) {
+        const cell = document.createElement('div');
+        cell.classList.add('cell');
+
+        gridContainer.appendChild(cell);
+    }
+}
 
 toggleBorders.addEventListener('click', function() {
     if (gridContainer.classList.contains('withBorders')) {
@@ -12,25 +37,13 @@ toggleBorders.addEventListener('click', function() {
     }
 });
 
-document.addEventListener('DOMContentLoaded', function() {
-    const gridContainer = document.getElementById('gridContainer');
+function updateCellBackgroundColor(color)   {
+    document.documentElement.style.setProperty('--selected-color', color);
+}
 
-    // Define grid size here
-    const gridSize = 16; // Change this value for different grid sizes
-    const cellSize = 50; // This is the pixel size of each cell
-
-    gridContainer.style.gridTemplateColumns = `repeat(${gridSize}, ${cellSize}px)`;
-    gridContainer.style.gridTemplateRows = `repeat(${gridSize}, ${cellSize}px)`;
-    gridContainer.style.width = `${gridSize * cellSize}px`;
-    gridContainer.style.height = `${gridSize * cellSize}px`;
-
-    for (let i = 0; i < gridSize * gridSize; i++) {
-        const cell = document.createElement('div');
-        cell.classList.add('cell');
-
-        cell.addEventListener('mouseover', function() {
-            cell.style.backgroundColor = 'orange'; 
-        });
-        gridContainer.appendChild(cell);
-    }
+colorPicker.addEventListener('change', function(event) {
+    const selectedColor = event.target.value;
+    updateCellBackgroundColor(selectedColor);
 });
+
+createGrid();
